ec88283552f50272e4da1b1f9a99e5210352546a
Brian Demant <brian.demant@gmail.com>
2019-03-31 20:39:00 +0200

    esnext ftw

--- commands/main/src/hello.ts
+++ commands/main/src/hello.ts
@@ -2,8 +2,10 @@ function start() {
 	const args = [...process.argv]
 	args.shift() // node
 	args.shift() // file
-	const qwe: number = 0
-	if (args.length > qwe) {
+
+	const magicNumber = 0
+
+	if (args.length > magicNumber) {
 		console.log("hello", args.join(" "))
 	} else {
 		console.log("hello world!!")
--- libs/point/src/advanced.ts
+++ libs/point/src/advanced.ts
@@ -1,11 +1,12 @@
 export class Advanced {
+	private readonly prop: number = 42
 	constructor(public qwe: string) {
-		Advanced.hey(this)
+		Advanced.hey(this, this.prop)
 	}
 
-	static readonly hey = async (arg: Advanced) => {
+	static readonly hey = async (arg: Advanced, prop: number) => {
 		// this is a static function
-		console.log(arg)
+		console.log(arg, prop)
 	}
 }
 
